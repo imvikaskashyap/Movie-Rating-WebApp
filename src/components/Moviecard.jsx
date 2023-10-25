53481e3e82311a2fde1c515dd1ecd7b0d2f273ea
imvikaskashyap <mvikaskashyap@gmail.com>
2023-10-25 22:53:46 +0530

    animation on hover

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -9,6 +9,7 @@ function Moviecard({ movie }) {
 	const [localRating, setLocalRating] = useState(movie.rating);
 	const rating = useSelector((state) => state.handleRating);
 	const [addedToQueue, setAddedToQueue] = useState(false);
+	const [isShown, setIsShown] = useState(false);
 
 	const addMovieToQueue = (movie) => {
 		if (!addedToQueue) {
@@ -38,55 +39,55 @@ function Moviecard({ movie }) {
 	const stars = Array(5).fill(0);
 
 	return (
-		<>
-			<div className="w-80 text-white max-w-sm  bg-white border border-gray-200 m-6 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-				<a href="#" className="m-auto">
-					<img
-						className="p-4 rounded-t-lg m-auto movie-poster backdrop-blur-sm hover:backdrop-blur-lg"
-						style={{ opacity: "0.8" }}
-						src={movie.Poster || noimage}
-						alt="Movie poster"
-					/>
-				</a>
-				<div className="px-5 pb-5">
-					<h5 className="text-xl font-semibold tracking-tight text-white dark-text-white">
-						{movie.Title}
-					</h5>
-					<div className="flex items-center mt-2.5 mb-5 justify-between">
-						<div className="flex items-center justify-between">
-							<button
-								className=" text-4xl text-red-600"
-								onClick={handleDecrement}>
-								-
-							</button>
-							{stars.map((_, index) => (
-								<span
-									key={index}
-									className={`text-2xl m-2 ${
-										index < localRating
-											? "text-yellow-300"
-											: "text-gray-200 dark-text-gray-600"
-									}`}>
-									{index < localRating ? "★" : "☆"}{" "}
-								</span>
-							))}
-							<button
-								className="text-4xl text-green-500"
-								onClick={handleIncrement}>
-								{" "}
-								+{" "}
-							</button>
-						</div>
-						<span className="bg-blue-100 text-blue-800 text-lg font-semibold mr-2 px-2.5 py-0.5 rounded dark-bg-blue-200 dark-text-blue-800 ml-3">
-							{localRating}
-						</span>
+		<div
+			className={`w-80 text-white max-w-sm  bg-white border border-gray-200 m-6 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 movie-card ${
+				isShown ? "show" : ""
+			}`}
+			onMouseEnter={() => setIsShown(true)}>
+			<img
+				className="p-4 rounded-t-lg m-auto movie-poster backdrop-blur-sm hover:backdrop-blur-lg img"
+				style={{ opacity: "0.8" }}
+				src={movie.Poster || noimage}
+				alt="Movie poster"
+			/>
+			<div className="px-5 pb-5">
+				<h5 className="text-xl font-semibold tracking-tight text-white dark-text-white">
+					{movie.Title}
+				</h5>
+				<div className="flex items-center mt-2.5 mb-5 justify-between">
+					<div className="flex items-center justify-between">
+						<button
+							className=" text-4xl text-red-600"
+							onClick={handleDecrement}>
+							-
+						</button>
+						{stars.map((_, index) => (
+							<span
+								key={index}
+								className={`text-2xl m-2 ${
+									index < localRating
+										? "text-yellow-300"
+										: "text-gray-200 dark-text-gray-600"
+								}`}>
+								{index < localRating ? "★" : "☆"}{" "}
+							</span>
+						))}
+						<button
+							className="text-4xl text-green-500"
+							onClick={handleIncrement}>
+							{" "}
+							+{" "}
+						</button>
 					</div>
-					<div className="flex items-center justify-center">
-						<Btn item="Add to Queue" onClick={() => addMovieToQueue(movie)} />
-					</div>
+					<span className="bg-blue-100 text-blue-800 text-lg font-semibold mr-2 px-2.5 py-0.5 rounded dark-bg-blue-200 dark-text-blue-800 ml-3">
+						{localRating}
+					</span>
+				</div>
+				<div className="flex items-center justify-center">
+					<Btn item="Add to Queue" onClick={() => addMovieToQueue(movie)} />
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
