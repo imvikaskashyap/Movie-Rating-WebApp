cf14064e1ba6e8732f30e30908aec3d91ba12a3e
imvikaskashyap <mvikaskashyap@gmail.com>
2023-10-26 10:35:39 +0530

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import logo from "../assets/images/icons8-movie-projector-64.png";
 
 function Header() {
 	const navigate = useNavigate();
 
 	return (
 		<header
-			className="p-4 flex items-center justify-evenly top-0 sticky z-50"
+			className="p-4 flex items-center justify-around top-0 sticky z-50"
 			style={{ backgroundColor: "#5e5d5d" }}>
-			<div className="container mx-auto">
+			<div className="container m-auto flex flex-col items-start justify-center">
+				<img src={logo} alt="logo" className="ml-14" />
 				<h1
 					className="text-2xl font-bold text-white cursor-pointer"
 					onClick={() => navigate("/")}>
 					Movie Rating App
 				</h1>
 			</div>
-			<Link className="text-white no-underline w-20" to="/all-movies">
+			<Link className="text-white no-underline " to="/all-movies">
 				All Movies
 			</Link>
-			<Link className="text-white no-underline w-20" to="/my-queue">
+			<Link className="text-white no-underline " to="/my-queue">
 				My Queue
 			</Link>
 		</header>
